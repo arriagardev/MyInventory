48e9a4b31cbb1e3847ae13805e8f793fde0bffe8
Juan Arriaga <juan.arriaga@flextronics.com>
2017-02-28 10:03:34 -0600

    Initial Commit

---
+++ MyInventory/Models/ProductModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyInventory.Models
+{
+    public class ProductModels
+    {
+        public string Name { get; set; }
+        public float Cost { get; set; }
+    }
+}
